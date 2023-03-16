07cfda89bfa8d475357668cda09fa7a64f4c211a
Pachanat <51911918+Pachanat@users.noreply.github.com>
2023-03-16 09:59:02 +0100

    Initialisation du projet avec https://start.spring.io/
    # Projet : Maven
    # Langage : Java
    # Spring Boot : 2.7.9
    # Project Metadata :
    - Group : isen.group1
    - Artifact : api.assurance
    - Name : api.assurance
    - Description : TP Noté groupe 1 Sylène, Nathan LD, Mathis, Thomas, Henrick
    - Package name : isen.group1.api.assurance
    - Packaging : Jar
    - Java : 17
    # Dépendances :
    - Spring Web
    - MySQL Driver
    - Spring Data JPA

--- README.md
+++
@@ -1,3 +0,0 @@
-# TPBanque
-
-nathan was here
---
+++ src/main/java/isen/group1/api/assurance/Application.java
@@ -0,0 +1,13 @@
+package isen.group1.api.assurance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
