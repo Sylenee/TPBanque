b36912574894813a459754f4cf5cb08f7ba26371
thomasgouyet29@gmail.com <thomasgouyet29@gmail.com>
2023-03-16 14:04:01 +0100

    Create Client service and client repository

--- src/main/java/isen/group1/api/assurance/data/repository/ClientRepository.java
+++ src/main/java/isen/group1/api/assurance/data/repository/ClientRepository.java
@@ -1,4 +1,23 @@
 package isen.group1.api.assurance.data.repository;
 
-public interface ClientRepository {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ClientRepository implements ClientRepositoryInterface{
+	
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	
+	public ClientRepository() {
+		super();
+	}
+	
+	@Override
+	public Integer nombreDePersonne() {
+		String sql = "SELECT COUNT(*) FROM client";
+		return this.jdbcTemplate.queryForObject(sql,Integer .class);
+	}
+
 }
