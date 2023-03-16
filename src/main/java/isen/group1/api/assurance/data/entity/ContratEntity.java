016f77df17a27eddd02dfaeca3f1e9eb3f299c4f
firnen <mathisg29@gmail.com>
2023-03-16 11:04:04 +0100

    Contrat Entity fix + Contrat DTO

--- src/main/java/isen/group1/api/assurance/data/entity/ContratEntity.java
+++ src/main/java/isen/group1/api/assurance/data/entity/ContratEntity.java
@@ -4,12 +4,16 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.io.Serial;
 import java.io.Serializable;
 import java.sql.Timestamp;
 
 @Entity
 @Table(name = "CONTRAT")
 public class ContratEntity implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
 
     @Id
     @Column(name = "id")
