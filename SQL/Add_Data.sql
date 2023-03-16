2af72a1b649087283802abebe78c85cda0b83334
HenrikLidstrom <henrik.lidstrom@isen-ouest.yncrea.fr>
2023-03-16 14:39:29 +0100

    id

--- SQL/Add_Data.sql
+++ SQL/Add_Data.sql
@@ -1,18 +1,18 @@
 --populate tables for all to use locally
 
 INSERT INTO client(id, nom, prenom, adresse, id_contrat)
-    VALUES('Le Duot','Nathan','1 rue du Forrestier 22000 St Brieuc','1'),
-    ('Dupont','François','4 rue du Boucher 22000 St Brieuc','2'),
-    ('Portus','Jean','7 rue du Boulanger 22000 St Brieuc','3'),
-    ('Tourret','Jacques','10 rue du Patissier 22000 St Brieuc','4'),
-    ('Renault','Danielle','13 rue de la Gendarmerie 22000 St Brieuc','5'),
-    ('Noutu','Gabrielle','16 rue De Gaule 22000 St Brieuc','6'),
-    ('Opieds','Maëlle','19 rue Vaut 22000 St Brieuc','8'),
-    ('Spears','Britney','22 rue du Chanteur 22000 St Brieuc','7'),
-    ('Obama','Barrack','22 rue du President 22000 St Brieuc','9'),
-    ('Eye Pees','Black','22 rue du Groupe 22000 St Brieuc','10'),
-    ('Mile','Eight','22 rue du Rap 22000 St Brieuc','11'),
-    ('Jackson','Janette','22 rue de l Histoire 22000 St Brieuc','12')
+    VALUES('1','Le Duot','Nathan','1 rue du Forrestier 22000 St Brieuc','1'),
+    ('2','Dupont','François','4 rue du Boucher 22000 St Brieuc','2'),
+    ('3','Portus','Jean','7 rue du Boulanger 22000 St Brieuc','3'),
+    ('4','Tourret','Jacques','10 rue du Patissier 22000 St Brieuc','4'),
+    ('5','Renault','Danielle','13 rue de la Gendarmerie 22000 St Brieuc','5'),
+    ('6','Noutu','Gabrielle','16 rue De Gaule 22000 St Brieuc','6'),
+    ('7','Opieds','Maëlle','19 rue Vaut 22000 St Brieuc','8'),
+    ('8','Spears','Britney','22 rue du Chanteur 22000 St Brieuc','7'),
+    ('9','Obama','Barrack','22 rue du President 22000 St Brieuc','9'),
+    ('10','Eye Pees','Black','22 rue du Groupe 22000 St Brieuc','10'),
+    ('11','Mile','Eight','22 rue du Rap 22000 St Brieuc','11'),
+    ('12','Jackson','Janette','22 rue de l Histoire 22000 St Brieuc','12')
 ;
 
 INSERT INTO conseiller(id, nom, prenom)
