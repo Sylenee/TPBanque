b313ee752eadd73317a6407484d63800e339dcee
HenrikLidstrom <henrik.lidstrom@isen-ouest.yncrea.fr>
2023-03-16 11:22:39 +0100

    added conseiller

--- SQL/Add_Data.sql
+++ SQL/Add_Data.sql
@@ -1,6 +1,6 @@
 --populate tables for all to use locally
 
-INSERT INTO client(id,nom,prenom,adresse,id_contrat)
+INSERT INTO client(id, nom, prenom, adresse, id_contrat)
     -> VALUES
     ->('Le Duot','Nathan','1 rue du Forrestier 22000 St Brieuc','1')
     ->('Dupont','François','4 rue du Boucher 22000 St Brieuc','2')
@@ -15,5 +15,14 @@ INSERT INTO client(id,nom,prenom,adresse,id_contrat)
     ->('Mile','Eight','22 rue du Rap 22000 St Brieuc','11')
     ->('Jackson','Janette','22 rue de l Histoire 22000 St Brieuc','12')
 ;
+
+INSERT INTO conseiller(id, nom, prenom)
+    ->VALUES
+    ->('Martin','Pierre',1)
+    ->('Dubois','Marie',1)
+    ->('Leroy','Antoine',1)
+    ->('Chevalier','Emmanuelle',1)
+;
+
 
 
