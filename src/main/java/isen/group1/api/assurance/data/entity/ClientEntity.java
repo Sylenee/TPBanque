dedbaaa17048b2b5fda1579b9f25bf755e5b71a6
thomasgouyet29@gmail.com <thomasgouyet29@gmail.com>
2023-03-16 11:07:30 +0100

    Add ClientEntity.java and ClientDTO.java

--- src/main/java/isen/group1/api/assurance/data/entity/ClientEntity.java
+++ src/main/java/isen/group1/api/assurance/data/entity/ClientEntity.java
@@ -1,4 +1,81 @@
 package isen.group1.api.assurance.data.entity;
 
-public class ClientEntity {
+import java.io.Serializable;
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ClientEntity implements Serializable {
+
+	private Integer id;
+	private String nom;
+	private String prenom;
+	private String adresse;
+	private List<ContratEntity> contrats;
+
+	/**
+	 * @return the id
+	 */
+	public Integer getId() {
+		return this.id;
+	}
+
+	public List<ContratEntity> getContrats() {
+		return contrats;
+	}
+
+	public void setContrats(List<ContratEntity> contrats) {
+		this.contrats = contrats;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(final Integer id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the nom
+	 */
+	public String getNom() {
+		return this.nom;
+	}
+
+	/**
+	 * @param nom the nom to set
+	 */
+	public void setNom(final String nom) {
+		this.nom = nom;
+	}
+
+	/**
+	 * @return the prenom
+	 */
+	public String getPrenom() {
+		return this.prenom;
+	}
+
+	/**
+	 * @param prenom the prenom to set
+	 */
+	public void setPrenom(final String prenom) {
+		this.prenom = prenom;
+	}
+
+	/**
+	 * @return the adresse
+	 */
+	public String getAdresse() {
+		return this.adresse;
+	}
+
+	/**
+	 * @param adresse the adresse to set
+	 */
+	public void setAdresse(final String adresse) {
+		this.adresse = adresse;
+	}
+
 }
--- src/main/java/isen/group1/api/assurance/model/dto/ClientDTO.java
+++ src/main/java/isen/group1/api/assurance/model/dto/ClientDTO.java
@@ -1,4 +1,89 @@
 package isen.group1.api.assurance.model.dto;
 
-public class ClientDTO {
+import java.io.Serializable;
+
+import isen.group1.api.assurance.data.entity.ClientEntity;
+
+
+public class ClientDTO implements Serializable {
+
+	private Integer id;
+	private String nom;
+	private String prenom;
+	private String adresse;
+
+	public ClientDTO() {
+		super();
+	}
+	public ClientDTO(final ClientEntity clientEntity) {
+		this.id = clientEntity.getId();
+		this.nom = clientEntity.getNom();
+		this.prenom = clientEntity.getPrenom();
+		this.adresse = clientEntity.getAdresse();
+	}
+
+	public ClientEntity toEntity(){
+		ClientEntity clientEntity = new ClientEntity();
+		clientEntity.setId(id);
+		clientEntity.setNom(nom);
+		clientEntity.setPrenom(prenom);
+		clientEntity.setAdresse(adresse);
+		return clientEntity;
+	}
+	/**
+	 * @return the id
+	 */
+	public Integer getId() {
+		return this.id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(final Integer id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the nom
+	 */
+	public String getNom() {
+		return this.nom;
+	}
+
+	/**
+	 * @param nom the nom to set
+	 */
+	public void setNom(final String nom) {
+		this.nom = nom;
+	}
+
+	/**
+	 * @return the prenom
+	 */
+	public String getPrenom() {
+		return this.prenom;
+	}
+
+	/**
+	 * @param prenom the prenom to set
+	 */
+	public void setPrenom(final String prenom) {
+		this.prenom = prenom;
+	}
+
+	/**
+	 * @return the adresse
+	 */
+	public String getAdresse() {
+		return this.adresse;
+	}
+
+	/**
+	 * @param adresse the adresse to set
+	 */
+	public void setAdresse(final String adresse) {
+		this.adresse = adresse;
+	}
+
 }
