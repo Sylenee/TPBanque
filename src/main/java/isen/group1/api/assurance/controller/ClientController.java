8ac2fb61b50e4abbb9ad9726be073c123c1d941e
Pachanat <51911918+Pachanat@users.noreply.github.com>
2023-03-17 14:18:31 +0100

    suppression lib

--- src/main/java/isen/group1/api/assurance/controller/ClientController.java
+++ src/main/java/isen/group1/api/assurance/controller/ClientController.java
@@ -2,6 +2,12 @@ package isen.group1.api.assurance.controller;
 
 import java.util.List;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,19 +21,32 @@ import isen.group1.api.assurance.service.ClientService;
 
 @RestController
 @RequestMapping("/clients")
+@Tag(name = "Clients", description = "Endpoints pour les opérations sur les clients")
 public class ClientController {
-	
+
 	@Autowired
 	ClientService clientService;
-		
+
 	@GetMapping("/{client_id}/contrats")
+	@Operation(summary = "Obtenir tous les contrats d'un client", description = "Récupère la liste de tous les contrats d'un client avec l'identifiant donné", tags = {"Clients"})
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Liste des contrats du client retournée", content = @Content(schema = @Schema(implementation = ContratDTO.class))),
+			@ApiResponse(responseCode = "404", description = "Client non trouvé", content = @Content(schema = @Schema(implementation = Exception.class))),
+			@ApiResponse(responseCode = "500", description = "Raison inconnu", content = @Content(schema = @Schema(implementation = Exception.class)))
+	})
 	public ResponseEntity<List<ContratDTO>> GetContratsFromClient(@PathVariable("client_id") Integer idClient) throws Exception {
 		List<ContratDTO> listContrats = clientService.getAllContratsFromClientID(idClient);
 		return ResponseEntity.status(HttpStatus.CREATED).body(listContrats);
 
 	}
-	
+
 	@GetMapping("/{client_id}/contrats/{contrat_id}")
+	@Operation(summary = "Obtenir un contrat d'un client", description = "Récupère un contrat d'un client avec les identifiants donnés", tags = {"Clients"})
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Contrat du client retourné", content = @Content(schema = @Schema(implementation = ContratDTO.class))),
+			@ApiResponse(responseCode = "404", description = "Client ou contrat non trouvé", content = @Content(schema = @Schema(implementation = Exception.class))),
+			@ApiResponse(responseCode = "500", description = "Raison inconnu", content = @Content(schema = @Schema(implementation = Exception.class)))
+	})
 	public ResponseEntity<ContratDTO> GetOneContratsFromClient(@PathVariable("client_id") Integer idClient, @PathVariable("contrat_id") Integer idContrat) throws Exception {
 		ContratDTO contrat = clientService.getOneContratFromClientID(idClient, idContrat);
 		return ResponseEntity.status(HttpStatus.CREATED).body(contrat);
--- src/main/java/isen/group1/api/assurance/controller/ConseillerController.java
+++ src/main/java/isen/group1/api/assurance/controller/ConseillerController.java
@@ -1,11 +1,13 @@
 package isen.group1.api.assurance.controller;
 
 
+import io.swagger.v3.oas.annotations.Operation;
 import isen.group1.api.assurance.model.dto.ClientDTO;
 import isen.group1.api.assurance.model.dto.ContratDTO;
 import isen.group1.api.assurance.service.ConseillerService;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,17 +17,20 @@ import java.util.List;
 @RestController
 @RequestMapping("/conseiller")
 @AllArgsConstructor
+@Tag(name = "Conseiller", description = "Endpoints pour les opérations sur les conseillers")
 public class ConseillerController {
 
     @Autowired
     private ConseillerService conseillerService;
 
     @GetMapping("/{idConseiller}/listClient")
+    @Operation(summary = "Récupère la liste des clients d'un conseiller", description = "Récupère la liste des clients d'un conseiller à partir de l'ID du conseiller.")
     public ResponseEntity<List<ClientDTO>> getListClient(@PathVariable("idConseiller")Integer idConseiller) throws Exception{
         return ResponseEntity.status(HttpStatus.CREATED).body(conseillerService.getListClient(idConseiller));
     }
 
     @DeleteMapping("/{idConseiller}/supprimer/client/{idClient}")
+    @Operation(summary = "Supprime un client d'un conseiller", description = "Supprime un client d'un conseiller à partir de l'ID du conseiller et de l'ID du client.")
     public ResponseEntity<Object> deleteClient(@PathVariable("idConseiller")Integer idConseiller, @PathVariable("idClient")Integer idClient) throws Exception {
         conseillerService.deleteClient(idConseiller, idClient);
         return ResponseEntity.status(HttpStatus.ACCEPTED).build();
@@ -37,12 +42,14 @@ public class ConseillerController {
     }
 
     @PutMapping("/{idConseiller}/modifier/client/{idClient}")
+    @Operation(summary = "Met à jour un client d'un conseiller", description = "Met à jour un client d'un conseiller à partir de l'ID du conseiller, l'ID du client et des informations de mise à jour du client.")
     public ResponseEntity<ClientDTO> updateClient(@PathVariable("idConseiller")Integer idConseiller, @RequestBody ClientDTO clientModif,@PathVariable("idClient") Integer idClient) throws Exception {
         conseillerService.updateClient(idConseiller, clientModif, idClient);
         return ResponseEntity.status(HttpStatus.OK).body(clientModif);
     }
 
     @PutMapping("{idconseiller}/modifier/contrat/{idcontrat}")
+    @Operation(summary = "Met à jour un contrat d'un conseiller", description = "Met à jour un contrat d'un conseiller à partir de l'ID du conseiller, l'ID du contrat et des informations de mise à jour du contrat.")
     public ResponseEntity<ContratDTO> updateContrat(@RequestBody ContratDTO contrat, @PathVariable("idcontrat") Integer idcontrat,@PathVariable("idconseiller") Integer idConseiller) throws Exception {
         contrat.setId(idcontrat);
         ContratDTO contratDTO = this.conseillerService.updateContrat(idcontrat, contrat, idConseiller);
@@ -50,6 +57,7 @@ public class ConseillerController {
     }
 
     @PostMapping("{idconseiller}/ajouter/contrat/{idclient}")
+    @Operation(summary = "Ajouter un contrat", description = "Ajoute un nouveau contrat pour un client donné et un conseiller donné")
     public ResponseEntity<ContratDTO> addContrat(@RequestBody ContratDTO contrat, @PathVariable("idclient") Integer idclient,@PathVariable("idconseiller") Integer idConseiller) throws Exception {
         contrat.setIdClient(idclient);
         ContratDTO contratDTO = this.conseillerService.ajouterContrat(contrat, idConseiller);
